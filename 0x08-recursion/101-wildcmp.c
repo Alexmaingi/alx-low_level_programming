5dc2de23adc6564ecda3b14b24aaba220c864386
Alexmaingi <alexmaingi077@gmail.com>
2023-04-05 12:38:00 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,23 +1,32 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - a function that returns the length of a string
- * @s: the string character
- *
- * Return: integer value
+ * wildcmp - Compare strings
+ * @s1: pointer to string params
+ * @s2: pointer to string params
+ * Return: 0
  */
-int _strlen_recursion(char *s)
+
+int wildcmp(char *s1, char *s2)
 {
-	int len = 0;
-
-	if (*s == '\0')
-		return (0);
-
-	else
+	if (*s1 == '\0')
 	{
-		len += _strlen_recursion(s + 1);
-		len++;
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
 	}
 
-	return (len);
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
 }
+Footer
+
